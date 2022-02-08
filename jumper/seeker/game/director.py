096aae055d376a1feeb90d3fccca16116e9267b0
Ken Kyle <88218089+klhkyle@users.noreply.github.com>
2022-02-07 22:25:56 -0700

    Update director.py

--- jumper/seeker/game/director.py
+++ jumper/seeker/game/director.py
@@ -1,66 +1,35 @@
-from game.terminal_service import TerminalService
-from game.parachute import Parachute
 from game.words import Words
+from game.parachute import Parachute
 
 
 class Director:
-    """A person who directs the game. 
-    
-    The responsibility of a Director is to control the sequence of play.
-
-    Attributes:
-        hider (Hider): The game's hider.
-        is_playing (boolean): Whether or not to keep playing.
-        seeker (Seeker): The game's seeker.
-        terminal_service: For getting and displaying information on the terminal.
-    """
 
     def __init__(self):
-        """Constructs a new Director.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
-        self._hider = Hider()
         self._is_playing = True
-        self._seeker = Seeker()
-        self._terminal_service = TerminalService()
+        self._words = Words()
+        self._parachute = Parachute()
         
     def start_game(self):
-        """Starts the game by running the main game loop.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
         while self._is_playing:
-            self._get_inputs()
-            self._do_updates()
-            self._do_outputs()
+            self._get_word()
+            self._display_hidden_word()
+            self._display_parachute()
+            self._letter_input()
 
-    def _get_inputs(self):
-        """Moves the seeker to a new location.
+            self._end_game()
 
-        Args:
-            self (Director): An instance of Director.
-        """
-        new_location = self._terminal_service.read_number("\nEnter a location [1-1000]: ")
-        self._seeker.move_location(new_location)
+    def _get_word(self):
+        new_word = self._words.get_word()
+        print(f"The new word is {new_word}")  # DELETE THIS WHE COMPLETE
+
+    def _display_hidden_word(self):
+        Words().hide_word()
+
+    def _display_parachute(self):
+        print(Parachute().draw_parachute())
         
-    def _do_updates(self):
-        """Keeps watch on where the seeker is moving.
+    def _letter_input(self):
+        Words().get_letters()
 
-        Args:
-            self (Director): An instance of Director.
-        """
-        self._hider.watch_seeker(self._seeker)
-        
-    def _do_outputs(self):
-        """Provides a hint for the seeker to use.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        hint = self._hider.get_hint()
-        self._terminal_service.write_text(hint)
-        if self._hider.is_found():
-            self._is_playing = False
+    def _end_game(self):
+        self._is_playing = False
