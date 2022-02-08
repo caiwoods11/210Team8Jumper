492d1060424b270f65660c020cc336fb20222f00
Ken Kyle <88218089+klhkyle@users.noreply.github.com>
2022-02-07 22:26:36 -0700

    Update parachute.py

--- jumper/seeker/game/parachute.py
+++ jumper/seeker/game/parachute.py
@@ -1,30 +1,15 @@
-# TODO: Implement the Seeker class as follows...
-import random
-
-from matplotlib.pyplot import draw
-
-# 1) Add the class declaration. Use the following class comment.
 class Parachute:
-    """Create the layout of the parachute guy. 
-    """
 
-# 2) Create the class constructor. Use the following method comment.
     def __init__(self):
 
         """Constructs a new Parachute class.
-
         Args:
             self (Parachute): An instance of Parachute.
         """
 
         self.p_parts = ["_____","/","_____","\\","\\","/","\\","/","O"]
 
-# 3) Create the get_location(self) method. Use the following method comment.
     def draw_parachute(self):
-        """Gets the current location.
-        Returns:
-            number: The current location,
-        """
         self.p_draw = f"""
       {self.p_parts[0]}
      {self.p_parts[1]}{self.p_parts[2]}{self.p_parts[3]}
@@ -33,9 +18,9 @@ class Parachute:
         {self.p_parts[8]}
        /|\\
        / \\
-
     ^^^^^^^^^ """
         return self.p_draw
+
 
 
 # To test Parachute        
